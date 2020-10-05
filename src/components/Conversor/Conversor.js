5748ba8d1864dae62498e273d8589d389abecfa9
Vinicius Pereira de Oliveira <agavi2014@hotmail.com>
2020-10-05 13:16:57 -0300

    Primeiro app  em React-JS

---
+++ src/components/Conversor/Conversor.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+
+export default class Conversor extends Component {
+
+    constructor(props)
+    {
+        super(props);
+
+        this.state = {
+            moedaA_valor : "",
+            moedaB_valor : 0
+        }
+
+        this.converter = this.converter.bind(this);
+    }
+
+    converter()
+    {
+        let de_para = `${this.props.moedaA}_${this.props.moedaB}`
+        let url = `https://free.currconv.com/api/v7/convert?q=${de_para}&compact=ultra&apiKey=4d1c18c1bf24e044dcba`
+
+        fetch(url)
+        .then(
+            res => {
+                return res.json()
+            }
+        )
+        .then(
+            json => {
+                let cotacao = json[de_para];
+                let moedaB_valor = (parseFloat(this.state.moedaA_valor * cotacao)).toFixed(2);
+                this.setState({moedaB_valor})
+            }
+        )
+    }
+
+    render() {
+        return (
+            <div className = "Conversor">
+                <h2>{this.props.moedaA} para {this.props.moedaB}</h2>
+                <input type="text" onChange= {(event) => {
+                    this.setState({moedaA_valor: event.target.value})
+                }}></input>
+                <input type="button" value="Converter" onClick={this.converter}></input>
+                <h2>{this.state.moedaB_valor}</h2>
+            </div>
+        )
+    }
+}
